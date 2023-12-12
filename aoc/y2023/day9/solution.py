b6e583d13e96c913d77fb57f7ad272e74db3352a
Matt Wilson <22526682+automationator@users.noreply.github.com>
2023-12-11 23:54:42 -0500

    Day 9

--- aoc/y2023/day9/solution.py
+++ aoc/y2023/day9/solution.py
@@ -1,7 +1,67 @@
 # https://adventofcode.com/2023/day/9
 
+
+def parse_numbers(input_text: str) -> list[list[int]]:
+    return list(list(int(n) for n in line.split()) for line in input_text.splitlines())
+
+
+def get_differences(numbers: list[int]) -> list[int]:
+    return list(numbers[i + 1] - numbers[i] for i in range(len(numbers) - 1))
+
+
+def get_next_number(history: list[list[int]]) -> int:
+    """
+    Starting with the final row of the history, get the final number and add it
+    to the final number of the previous row. Append that number to the previous row.
+    Repeat until the first row is reached. Return the final number of the first row.
+    """
+
+    for i in range(len(history) - 1, 0, -1):
+        history[i - 1].append(history[i][-1] + history[i - 1][-1])
+
+    return history[0][-1]
+
+
+def get_previous_number(history: list[list[int]]) -> int:
+    """
+    Starting with the final row of the history, get the first number and subtract it
+    from the first number of the previous row. Prepend that number to the previous row.
+    Repeat until the first row is reached. Return the first number of the first row.
+    """
+
+    for i in range(len(history) - 1, 0, -1):
+        history[i - 1].insert(0, history[i - 1][0] - history[i][0])
+
+    return history[0][0]
+
+
+def _build_history(numbers: list[int]) -> list[list[int]]:
+    """
+    Build a list of histories starting with the given numbers. Each history is a list
+    of lists of numbers. The first list is the given numbers. Each subsequent list is
+    the differences of the previous list. The final list is all zeroes.
+    """
+
+    history = [numbers]
+    while set(history[-1]) != {0}:
+        history.append(get_differences(history[-1]))
+
+    return history
+
+
 def part1(input_text: str) -> int:
-    return 0
+    result = 0
+    for numbers in parse_numbers(input_text):
+        history = _build_history(numbers)
+        result += get_next_number(history)
+
+    return result
+
 
 def part2(input_text: str) -> int:
-    return 0
+    result = 0
+    for numbers in parse_numbers(input_text):
+        history = _build_history(numbers)
+        result += get_previous_number(history)
+
+    return result
--- tests/aoc/y2023/day9/test_solution.py
+++ tests/aoc/y2023/day9/test_solution.py
@@ -5,12 +5,14 @@ from aoc.y2023.day9.solution import (
     part2,
 )
 
-def test_part1():
-    input_text = """"""
+input_text = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
 
-    assert part1(input_text) == 0
+
+def test_part1():
+    assert part1(input_text) == 114
+
 
 def test_part2():
-    input_text = """"""
-
-    assert part2(input_text) == 0
+    assert part2(input_text) == 2
